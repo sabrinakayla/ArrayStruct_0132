00f781db4de0011ff94f1ab41015578fc3f0a8de
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-07 17:38:06 +0700

    mengisi array

--- array.cpp
+++ array.cpp
@@ -10,3 +10,7 @@ int main(){
 
     // cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
+
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
