036b9e8040a78fb1ae57b3a216cd702e6592a6b9
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-07 18:49:48 +0700

    menambahkan struct

--- structgetline.cpp
+++ structgetline.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+struct detaiAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detaiAlamat alamat;
+    int umur;
+};
