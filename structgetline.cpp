5db1b5c359f847caa34c9c942380b8766a0eb8d8
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-07 18:51:41 +0700

    deklarasi variable struct

--- structgetline.cpp
+++ structgetline.cpp
@@ -13,3 +13,25 @@ struct Orang
     detaiAlamat alamat;
     int umur;
 };
+
+int main()
+{
+        //Deklarasi variabel struct
+        Orang mhs;
+        //Mengisi data
+        cout << "Isikan data nama : ";
+        getline(cin, mhs.nama);
+        cout << "Isikan data desa : ";
+        getline(cin, mhs.alamat.desa);
+        cout << "Isikan data kota : ";
+        getline(cin, mhs.alamat.kota);
+        cout << "Isikan data umur : ";
+        cin >> mhs.umur;
+        cout << endl;
+
+        cout << "Data Tampil" << endl;
+        cout << "Nama  : " << mhs.nama << endl;
+        cout << "Desa  : " << mhs.alamat.desa << endl;
+        cout << "Kota  : " << mhs.alamat.kota << endl;
+        cout << "Umur  : " << mhs.umur << endl;
+}
