9c3b6a4ca1538dd6a6fc153138541286a12eb651
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-07 18:40:20 +0700

    deklarasi dan mengisi data

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -13,3 +13,26 @@ struct Orang
     detaiAlamat alamat;
     int umur;
 };
+
+int main()
+{
+        {
+        //Deklarasi variabel struct
+        Orang mhs;
+        //Mengisi data
+        cout << "Isikan data nama : ";
+        cin >> mhs.nama;
+        cout << "Isikan data desa : ";
+        cin >> mhs.alamat.desa;
+        cout << "Isikan data kota : ";
+        cin >> mhs.alamat.kota;
+        cout << "Isikan data umur : ";
+        cin >> mhs.umur;
+        cout << endl;
+
+        cout << "Data Tampil" << endl;
+        cout << "Nama  : " << mhs.nama << endl;
+        cout << "Desa  : " << mhs.alamat.desa << endl;
+        cout << "Kota  : " << mhs.alamat.kota << endl;
+        cout << "Umur  : " << mhs.umur << endl;
+    }
